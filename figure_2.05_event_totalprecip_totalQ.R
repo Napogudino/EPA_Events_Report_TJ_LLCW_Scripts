428aee0b362dfa9f01b60f09ee2581f7f25883d0
tbiggsgithub <tbiggsgithub@users.noreply.github.com>
2017-05-22 12:57:32 -0700

    Update figure_2.05_event_totalprecip_totalQ.R

--- figure_2.05_event_totalprecip_totalQ.R
+++ figure_2.05_event_totalprecip_totalQ.R
@@ -4,13 +4,14 @@
 #Script written by Kris Taniguchi, SDSU (kristaniguchi@gmail.com)
 
 #Set working directory to the data folder, script directory will be used if sourcing functions
-getwd() #the directory where the script is saved
-setwd('../EPA_Events_Report_TJ_LLCW_Data') #set working directory as the data folder, which is one folder back in it's own folder
+# getwd() #the directory where the script is saved
+# setwd('../EPA_Events_Report_TJ_LLCW_Data') #set working directory as the data folder, which is one folder back in it's own folder
 
 ###############################################################################################################
 
 #Read in table 2.3 summary table of observed events
-data = read.csv("summary_table_obs_allevents.csv")
+# data = read.csv("summary_table_obs_allevents.csv")
+data = read.csv("https://raw.githubusercontent.com/kristaniguchi/EPA_Events_Report_TJ_LLCW_Data/master/summary_table_obs_allevents.csv")
 
 ###############################################################################################################
 
